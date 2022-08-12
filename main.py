50dfd1bc310d2c0615cc7e0e5a332b3940208643
SergeyRO <sergey_r.o@mail.ru>
2022-08-12 22:31:18 +0300

    two commit

--- main.py
+++ main.py
@@ -1,41 +1,44 @@
-def logger(other_function):
-    def log(*args, **kwargs):
-        function = other_function(*args, **kwargs)
+from datetime import datetime
 
-        print(f"Функция {other_function} Результат {other_function(*args, **kwargs)}")
-        print("Аргументы: ", *args, **kwargs)
-        return function
-    return log
+def logger(path):
+    def decorator(other_function):
+        def log(*args):
+            function = other_function(*args)
+            print(f"Дата и время вызова функции: {datetime.now()}")
+            print(f"Функция: {other_function}")
+            print("Аргументы: ", *args)
+            print(f'Результат: {function}')
+            with open(path, 'a', encoding='utf-8') as file:
+                file.write(f"{dict(data=f'{datetime.now()}', func=other_function, args=args, result=function)}\n")
+            return function
+        return log
+    return decorator
 
-
-@logger
+@logger('./logs/search_people.txt')
 # Поиск владельца по номеру документа
 def search_people(number_doc):
     for doc in documents:
         if doc['number'] == number_doc:
             return doc['name']
 
-
-@logger
+@logger('./logs/search_shelf.txt')
 # Поиск документа на полке
 def search_shelf(number_doc):
     for key, docs in directories.items():
         if number_doc in docs:
-            print(f"Документ находится на полке {key}")
-            break
-        else:
-            print("Документ отсутствует на какой-либо полке!")
-    return None
+            string = f"Документ находится на полке {key}"
+            return string
+    string = f"Данный документ отсутствует!"
+    return string
 
-
-@logger
+@logger('./logs/search_list.txt')
 # Список всех документов
 def search_list():
     for doc in documents:
         print(f"{doc['type']} \"{doc['number']}\" \"{doc['name']}\"")
+    return documents
 
-
-@logger
+@logger('./logs/add_doc.txt')
 # Добавление документа
 def add_doc(number_doc, type_doc, owner, shelf):
     val = directories.get(shelf, None)
@@ -43,13 +46,13 @@ def add_doc(number_doc, type_doc, owner, shelf):
         documents.append(dict(type=type_doc, number=number_doc, name=owner))
         val.append(number_doc)
         directories[shelf] = val
-        print(f"Документ добавлен в список и на полку {shelf}")
+        result = f"Документ добавлен в список и на полку {shelf}"
+        return result
     else:
-        print(f"Полка {shelf} отсутствует! Документ не добавлен!")
-    print(directories)
+        result = f"Полка {shelf} отсутствует! Документ не добавлен!"
+        return result
 
-
-@logger
+@logger('./logs/drop_doc.txt')
 # Удаление документа
 def drop_doc(number_doc):
     document = False
@@ -65,17 +68,18 @@ def drop_doc(number_doc):
             directory = True
             break
     if document and directory:
-        print("Документ удалён из списков и с полки")
+        result = "Документ удалён из списков и с полки"
+        return result
     else:
         if document and not directory:
-            print("Документ отсутствует на полке, но удалён из списка")
+            result = "Документ отсутствует на полке, но удалён из списка"
         elif not document and directory:
-            print("Документ отсутствует в списке, но удалён с полки")
+            result = "Документ отсутствует в списке, но удалён с полки"
         else:
-            print("Документ не существует")
+            result = "Документ не существует"
+        return result
 
-
-@logger
+@logger('./logs/modify_doc.txt')
 # Перемещение документа
 def modify_doc(number_doc, shelf):
     document = False
@@ -87,28 +91,31 @@ def modify_doc(number_doc, shelf):
                 directories[shelf].append(number_doc)
                 break
         if not document:
-            print(f"Документ {number_doc} не существует!")
+            res = f"Документ {number_doc} не существует!"
+            return res
         else:
-            print(f"Документ {number_doc} перенесён на полку {shelf}")
+            res = f"Документ {number_doc} перенесён на полку {shelf}"
+            return res
     else:
-        print(f"Полка {shelf} не существует!")
+        res = f"Полка {shelf} не существует!"
+        return res
 
-
-@logger
+@logger('./logs/add_shelf.txt')
 # Перемещение документа
 def add_shelf(shelf):
     if shelf in directories:
-        print(f"Полка {shelf} существует!")
+        res = f"Полка {shelf} существует!"
+        return res
     else:
         directories.setdefault(shelf, [])
-        print(f"Полка {shelf} добавлена!")
+        res = f"Полка {shelf} добавлена!"
+        return res
 
-
-@logger
+@logger('./logs/exit.txt')
 # Выход из программы
-def command_exit(*args, **kwargs):
-    quit()
-
+def command_exit():
+    res = "EXIT"
+    return res
 
 if __name__ == '__main__':
     documents = [
@@ -131,7 +138,7 @@ if __name__ == '__main__':
             print(search_people(number_doc))
         elif command == "s":
             number_doc = input("Введите номер документа: ")
-            search_shelf(number_doc)
+            print(search_shelf(number_doc))
         elif command == "l":
             search_list()
         elif command == "a":
@@ -139,18 +146,24 @@ if __name__ == '__main__':
             type_doc = input("Введите тип документа: ")
             owner = input("Введите владельца: ")
             shelf = input("Введите номер полки для хранения: ")
-            add_doc(number_doc, type_doc, owner, shelf)
+            print(add_doc(number_doc, type_doc, owner, shelf))
+            print(directories)
         elif command == "d":
             number_doc = input("Введите номер документа: ")
-            drop_doc(number_doc)
+            print(drop_doc(number_doc))
+            print(directories)
+            print(documents)
         elif command == "m":
             number_doc = input("Введите номер документа: ")
             shelf = input("Введите номер полки для хранения: ")
-            modify_doc(number_doc, shelf)
+            print(modify_doc(number_doc, shelf))
+            print(directories)
         elif command == "as":
             shelf = input("Введите номер полки для хранения: ")
-            add_shelf(shelf)
+            print(add_shelf(shelf))
+            print(directories)
         elif command == "q":
-            command_exit()
+            print(command_exit())
+            quit()
         else:
             print("Несуществующая команда!")
